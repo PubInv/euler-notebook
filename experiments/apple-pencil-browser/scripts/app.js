3a92004b9c34a54296694cafbcf4d4b4dde73734
David Jeschke <david@xtramath.org>
2019-03-14 21:24:06 -0700

    apple-pencil-browser refactoring

--- experiments/apple-pencil-browser/scripts/app.js
+++ experiments/apple-pencil-browser/scripts/app.js
@@ -1,124 +1,114 @@
+/*
+  apple-pencil-browser experiment
 
-var $force = document.querySelectorAll('#force')[0]
-var $touches = document.querySelectorAll('#touches')[0]
-var canvas = document.querySelectorAll('canvas')[0]
-var context = canvas.getContext('2d')
-var lineWidth = 0
-var isMousedown = false
-var points = []
+  Based on https://github.com/quietshu/apple-pencil-safari-api-test.
+*/
+(function(){
 
-canvas.width = window.innerWidth * 2
-canvas.height = window.innerHeight * 2
+  let context;
+  let lineWidth;
+  let isMouseDown;
+  let points;
 
-;['touchstart', 'mousedown'].forEach(function (ev) {
-  canvas.addEventListener(ev, function (e) {
-    var pressure = 0.1
-    var x, y
-    if (e.touches && e.touches[0] && typeof e.touches[0]["force"] !== "undefined") {
-      if (e.touches[0]["force"] > 0) {
-        pressure = e.touches[0]["force"]
-      }
-      x = e.touches[0].pageX * 2
-      y = e.touches[0].pageY * 2
-    } else {
-      pressure = 1.0
-      x = e.pageX * 2
-      y = e.pageY * 2
+  function domLoaded() {
+    // const $force = document.querySelectorAll('#force')[0]
+    // const $touches = document.querySelectorAll('#touches')[0]
+    const canvas = document.querySelectorAll('canvas')[0];
+
+    context = canvas.getContext('2d');
+    lineWidth = 0;
+    isMouseDown = false;
+    points = [];
+
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+
+    canvas.addEventListener('touchstart', onTouchStart);
+    canvas.addEventListener('mousedown', onTouchStart);
+
+    canvas.addEventListener('touchmove', onTouchMove);
+    canvas.addEventListener('mousemove', onTouchMove);
+
+    canvas.addEventListener('touchend', onTouchEnd);
+    canvas.addEventListener('touchleave', onTouchEnd);
+    canvas.addEventListener('mouseup', onTouchEnd);
+  }
+
+  function main() {
+    document.addEventListener("DOMContentLoaded", domLoaded);
+  }
+
+  function onTouchEnd(e) {
+    isMouseDown = false;
+    const { x, y, pressure } = xypFromTouchEvent(e);
+    context.strokeStyle = 'black';
+    context.lineCap = 'round';
+    context.lineJoin = 'round';
+    if (points.length >= 3) {
+      const l = points.length - 1;
+      context.quadraticCurveTo(points[l].x, points[l].y, x, y);
+      context.stroke();
     }
+    points = [];
+    lineWidth = 0;
+  }
 
-    isMousedown = true
-
-    // lineWidth = (pressure * 50 * 0.8 + lineWidth * 0.2)
-    context.lineWidth = lineWidth// pressure * 50;
-    context.strokeStyle = 'black'
-    context.lineCap = 'round'
-    context.lineJoin = 'round'
-    context.beginPath()
-    context.moveTo(x, y)
-
-    points.push({
-      x, y, lineWidth
-    })
-  })
-})
-
-;['touchmove', 'mousemove'].forEach(function (ev) {
-  canvas.addEventListener(ev, function (e) {
-    if (!isMousedown) return
-
-    var pressure = 0.1
-    var x, y
-    if (e.touches && e.touches[0] && typeof e.touches[0]["force"] !== "undefined") {
-      if (e.touches[0]["force"] > 0) {
-        pressure = e.touches[0]["force"]
-      }
-      x = e.touches[0].pageX * 2
-      y = e.touches[0].pageY * 2
-    } else {
-      pressure = 1.0
-      x = e.pageX * 2
-      y = e.pageY * 2
-    }
-
-    lineWidth = (Math.log(pressure + 1) * 40 * 0.4 + lineWidth * 0.6)
-    points.push({
-      x, y, lineWidth
-    })
-
-    context.strokeStyle = 'black'
-    context.lineCap = 'round'
-    context.lineJoin = 'round'
+  function onTouchMove(e) {
+    if (!isMouseDown) return;
+    const { x, y, pressure } = xypFromTouchEvent(e);
+    lineWidth = (Math.log(pressure + 1) * 40 * 0.4 + lineWidth * 0.6);
+    points.push({ x, y, lineWidth });
+    context.strokeStyle = 'black';
+    context.lineCap = 'round';
+    context.lineJoin = 'round';
     // context.lineWidth   = lineWidth// pressure * 50;
     // context.lineTo(x, y);
     // context.moveTo(x, y);
-
     if (points.length >= 3) {
-      var l = points.length - 1
-      var xc = (points[l].x + points[l - 1].x) / 2
-      var yc = (points[l].y + points[l - 1].y) / 2
-      context.lineWidth = points[l - 1].lineWidth
-      context.quadraticCurveTo(points[l - 1].x, points[l - 1].y, xc, yc)
-      context.stroke()
-      context.beginPath()
-      context.moveTo(xc, yc)
+      const l = points.length - 1;
+      const xc = (points[l].x + points[l - 1].x) / 2;
+      const yc = (points[l].y + points[l - 1].y) / 2;
+      context.lineWidth = points[l - 1].lineWidth;
+      context.quadraticCurveTo(points[l - 1].x, points[l - 1].y, xc, yc);
+      context.stroke();
+      context.beginPath();
+      context.moveTo(xc, yc);
     }
+    // $force.innerHTML = 'force = ' + pressure
+    // $touches.innerHTML = 'touchev = ' + (e.touches ? JSON.stringify(e.touches[0]) : '')
+    e.preventDefault();
+  }
 
-    $force.innerHTML = 'force = ' + pressure
-    $touches.innerHTML = 'touchev = ' + (e.touches ? JSON.stringify(e.touches[0]) : '')
-    e.preventDefault()
-  })
-})
+  function onTouchStart(e) {
+    isMouseDown = true;
+    const { x, y, pressure } = xypFromTouchEvent(e);
+    // lineWidth = (pressure * 50 * 0.8 + lineWidth * 0.2)
+    context.lineWidth = lineWidth;// pressure * 50;
+    context.strokeStyle = 'black';
+    context.lineCap = 'round';
+    context.lineJoin = 'round';
+    context.beginPath();
+    context.moveTo(x, y);
+    points.push({ x, y, lineWidth });
+  }
 
-;['touchend', 'touchleave', 'mouseup'].forEach(function (ev) {
-  canvas.addEventListener(ev, function (e) {
-    var pressure = 0.1
-    var x, y
-
+  function xypFromTouchEvent(e) {
+    let pressure = 0.1;
+    let x, y;
     if (e.touches && e.touches[0] && typeof e.touches[0]["force"] !== "undefined") {
       if (e.touches[0]["force"] > 0) {
-        pressure = e.touches[0]["force"]
+        pressure = e.touches[0]["force"];
       }
-      x = e.touches[0].pageX * 2
-      y = e.touches[0].pageY * 2
+      x = e.touches[0].pageX * 2;
+      y = e.touches[0].pageY * 2;
     } else {
-      pressure = 1.0
-      x = e.pageX * 2
-      y = e.pageY * 2
+      pressure = 1.0;
+      x = e.pageX * 2;
+      y = e.pageY * 2;
     }
+    return { x, y , pressure };
+  }
 
-    isMousedown = false
+  main();
 
-    context.strokeStyle = 'black'
-    context.lineCap = 'round'
-    context.lineJoin = 'round'
-
-    if (points.length >= 3) {
-      var l = points.length - 1
-      context.quadraticCurveTo(points[l].x, points[l].y, x, y)
-      context.stroke()
-    }
-
-    points = []
-    lineWidth = 0
-  })
-})
+})();
