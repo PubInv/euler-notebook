6538a5cd73c971e607ff7d878cd5fa4df82f416e
David Jeschke <djeschke@gmail.com>
2019-03-20 17:24:17 -0700

    Add a math block to the page

--- src/public/javascripts/app.js
+++ src/public/javascripts/app.js
@@ -5,19 +5,41 @@
   }
 
   function onDomReady(event){
-    const editorElement = document.getElementById('editor');
-    const applicationKey = editorElement.dataset.applicationkey;
-    const hmacKey = editorElement.dataset.hmackey;
-    const configuration = {
-      recognitionParams: {
-        type: 'TEXT',
-        protocol: 'WEBSOCKET',
-        apiVersion: 'V4',
-        server: { applicationKey, hmacKey }
-      }
-    }
-    console.dir(configuration);
-    MyScript.register(editorElement, configuration);
+
+    const htmlElement = document.documentElement;
+    const applicationKey = htmlElement.dataset.applicationkey;
+    const hmacKey = htmlElement.dataset.hmackey;
+    const baseRecoParams = {
+      protocol: 'WEBSOCKET',
+      apiVersion: 'V4',
+      server: { applicationKey, hmacKey },
+      v4: {
+        text: { 
+          guides: { enable: false } 
+        },
+        math: {
+          mimeTypes: ['application/x-latex', 'application/vnd.myscript.jiix']
+        },
+        }
+    };
+
+    const textBlock = document.getElementById('textblock');
+    textBlock.addEventListener('exported', onTextExported);
+    const textConfig = { recognitionParams: { ...baseRecoParams, type: 'TEXT' } };
+    MyScript.register(textBlock, textConfig);
+
+    const mathBlock = document.getElementById('mathblock');
+    mathBlock.addEventListener('exported', onMathExported);
+    const mathConfig = { recognitionParams: { ...baseRecoParams, type: 'MATH' } };
+    MyScript.register(mathBlock, mathConfig);
+  }
+
+  function onMathExported(event) {
+    console.dir(event);
+  }
+
+  function onTextExported(event) {
+    console.dir(event);
   }
 
   main();
