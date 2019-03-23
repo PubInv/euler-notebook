ce72c4f84cb994c4f01047340ae797e6ff4de91c
David <djeschke@gmail.com>
2019-03-22 21:40:14 -0700

    Add 'save' AJAX api call

--- src/public/javascripts/app.js
+++ src/public/javascripts/app.js
@@ -5,13 +5,16 @@
     protocol: 'WEBSOCKET',
     apiVersion: 'V4',
     v4: {
-      text: {
-        guides: { enable: false }
+      export: {
+        jiix: { strokes: true }
       },
       math: {
         mimeTypes: ['application/x-latex', 'application/vnd.myscript.jiix']
       },
-    }
+      text: {
+        guides: { enable: false }
+      },
+    },
   };
 
   // Globals
@@ -20,7 +23,7 @@
   // Functions
   //   in alphabetical order
 
-  function apiRequest(method) {
+  function apiGetRequest(method) {
     return new Promise((resolve, reject)=>{
       const xhr = new XMLHttpRequest();
       xhr.open('GET', `/api/${method}`);
@@ -30,13 +33,34 @@
             const results = JSON.parse(xhr.responseText);
             resolve(results);
           } catch (err) {
-            reject(new Error(`Invalid JSON returned from ${method} request: ${err.message}`));
+            reject(new Error(`Ajax GET /api/${method} returned invalid JSON: ${err.message}`));
           }
         } else {
-          reject(new Error(`${method} request failed with status of ${xhr.status}`));
+          reject(new Error(`Ajax GET /api/${method} request failed: status ${xhr.status}`));
         }
       };
       xhr.send();
+    });
+  }
+
+  function apiPostRequest(method, data) {
+    return new Promise((resolve, reject)=>{
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', `/api/${method}`);
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+          try {
+            const results = JSON.parse(xhr.responseText);
+            resolve(results);
+          } catch (err) {
+            reject(new Error(`Ajax POST /api/${method} returned invalid JSON: ${err.message}`));
+          }
+        } else {
+          reject(new Error(`Ajax POST /api/${method} request failed: status ${xhr.status}`));
+        }
+      };
+      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      xhr.send(JSON.stringify(data));
     });
   }
 
@@ -46,7 +70,8 @@
     return elt;
   }
 
-  function instantiateNotebook(notebook) {
+  // Instantiates the HTML DOM structure from a notebook object.
+  function deserializeNotebook(notebook) {
     // REVIEW: Assumes #pages element is empty. Assert or delete any existig children?
     const pagesElt = document.getElementById('pages');
     // REVIEW: Error if notebook has no pages?
@@ -57,11 +82,32 @@
         const layerElt = createDiv('layer');
         for (const blockData of layerData.blocks||[]) {
           const blockElt = createDiv('editor');
-          const blockConfig = { 
-            recognitionParams: { ...MYSCRIPT_RECO_PARAMS, server: myscriptKeys, type: blockData.type } 
+          blockElt.addEventListener('exported', onMyScriptExported);
+          const blockConfig = {
+            recognitionParams: { ...MYSCRIPT_RECO_PARAMS, server: myscriptKeys, type: blockData.type }
           };
           // REVIEW: Any problem registering the element before it is added to the DOM?
           MyScript.register(blockElt, blockConfig);
+          const editor = blockElt.editor;
+          switch (blockData.type) {
+          case 'MATH':
+            const jiix = blockData.jiix;
+            if (jiix) {
+              editor.import_(jiix, 'application/vnd.myscript.jiix');
+            }
+            break;
+          case 'TEXT':
+            const strokeGroups = blockData.strokeGroups;
+            if (strokeGroups) {
+              console.log("IMPORTING STROKE GROUPS");
+              console.dir(strokeGroups);
+              editor.eastereggs.importStrokeGroups(editor, strokeGroups);
+            }
+            break;
+          default:
+            // TODO: How to handle this error?
+            console.error(`Unexpected block type in deserialize: ${type}`);
+          }
           layerElt.appendChild(blockElt);
         }
         pageElt.appendChild(layerElt);
@@ -75,28 +121,83 @@
   }
 
   async function onDomReady(event){
-
     try {
+
+      // MyScript credentials are in the data-applicationkey and data-hmackey of the root <html> element.
+      // Store them in a global for future use.
       const htmlElement = document.documentElement;
       const applicationKey = htmlElement.dataset.applicationkey;
       const hmacKey = htmlElement.dataset.hmackey;
       myscriptKeys = { applicationKey, hmacKey };
 
-      const notebook = await apiRequest('open');
-      instantiateNotebook(notebook);
-      showStatusMessage('Notebook opened successfully.');
+      // Open a notebook
+      const openResults = await apiGetRequest('open');
+      // TODO: check openResults.ok
+      deserializeNotebook(openResults.notebook);
+      showStatusMessage("Notebook opened successfully.");
+
+      const saveButtonElt = document.getElementById('saveButton');
+      saveButtonElt.addEventListener('click', onSaveButtonClicked);
+
     } catch (err) {
       showErrorHeader("Error initializing page", err);
     }
   }
 
-  // function onMathExported(event) {
-  //   console.dir(event);
-  // }
+  function onMyScriptExported(event) {
+    const saveButtonElt = document.getElementById('saveButton');
+    saveButtonElt.disabled = false;
+  }
 
-  // function onTextExported(event) {
-  //   console.dir(event);
-  // }
+  async function onSaveButtonClicked(event) {
+    const notebook = serializeNotebook();
+    try {
+      await apiPostRequest('save', { notebook });
+    } catch(err) {
+      showErrorHeader("Error saving notebook", err);
+      return;
+    }
+    this.disabled = true;
+    showStatusMessage("Notebook saved successfully.");
+  }
+
+  // Returns the notebook object from the current HTML DOM.
+  function serializeNotebook() {
+    const pagesElt = document.getElementById('pages');
+    // REVIEW: It would be safer to only get the immediate descendants of the pagesElt.
+    const pageElts = pagesElt.getElementsByClassName('page');
+    const notebook = {
+      pages: Array.prototype.map.call(pageElts, function(pageElt){
+        const layerElts = pageElt.getElementsByClassName('layer');
+        return {
+          layers: Array.prototype.map.call(layerElts, function(layerElt){
+            const blockElts = layerElt.getElementsByClassName('editor');
+            return {
+              blocks: Array.prototype.map.call(blockElts, function(blockElt){
+                const editor = blockElt.editor;
+                const type = editor.configuration.recognitionParams.type;
+                switch(type) {
+                case 'MATH':
+                  const jiix = editor.exports && editor.exports['application/vnd.myscript.jiix'];
+                  return { type, jiix };
+                case 'TEXT':
+                  const strokeGroups = editor.model.strokeGroups;
+                  console.log("EXPORTING STROKE GROUPS");
+                  console.dir(strokeGroups);
+                  return { type, strokeGroups };
+                default:
+                  // TODO: How to handle this error?
+                  console.error(`Unexpected block type in serialize: ${type}`);
+                  return { type };
+                }
+              })
+            }
+          })
+        }
+      })
+    };
+    return notebook;
+  }
 
   function showErrorHeader(msg, err) {
     const errorHeader = document.getElementById('errorHeader');
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,22 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/open', function(req, res, next) {
-  res.json({
-    pages: [
-      {
-        layers: [
-          { 
-            blocks: [
-              { type: 'TEXT' },
-              { type: 'MATH' },
-            ]
-          }
-        ]
-      },
-      { /* empty page */ },
-    ]
-  });
+// Constants
+
+const NEW_NOTEBOOK = {
+  pages: [
+    {
+      layers: [
+        {
+          blocks: [
+            { type: 'TEXT' },
+            { type: 'MATH' },
+          ]
+        }
+      ]
+    },
+    { /* empty page */ },
+  ]
+};
+
+// Globals
+
+let notebook = NEW_NOTEBOOK;
+
+// Routes
+
+router.get('/open', function(_req, res, _next) {
+  console.log(`OPENING: ${JSON.stringify(notebook)}`);
+  res.json({ ok: true, notebook });
+});
+
+router.post('/save', function(req, res, _next) {
+  console.log(`SAVING: ${JSON.stringify(req.body)}`);
+  notebook = req.body.notebook;
+  res.json({ ok: true });
 });
 
 module.exports = router;
