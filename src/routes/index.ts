449a78b638cc3a0c2d929d8926ac4572569071ed
David Jeschke <david@xtramath.org>
2019-04-05 15:25:19 -0700

    Improved error handing in the web server.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -17,16 +17,60 @@ const credentials = getCredentials();
 // Routes
 
 router.get('/', async function(_req, res, _next) {
-  const userEntries = await getListOfUsers();
-  res.render('index', { userEntries });
+  try {
+    const messages = { banner: [ "Welcome to Math Tablet!" ], error: [] };
+    let userEntries = [];
+    try {
+      userEntries = await getListOfUsers();
+    } catch(err) {
+      switch(err.code) {
+      case 'ENOENT':
+        messages.error.push(`Math Tablet usr directory doesn't exist.`);
+        break;
+      default:
+        throw err;
+      }
+    }
+    res.render('index', { messages, userEntries });
+  } catch(err) {
+    console.error(err.message);
+    console.log(err.stack);
+    res.send(`Server crash: ${err.message}`);
+  }
 });
 
 router.get('/:user', async function(req, res, _next) {
-  const user: UserName = req.params.user;
-  const tDocEntries = await getListOfUsersNotebooks(user);
-  res.render('user-home', { user, tDocEntries });
+  try {
+    const messages = { error: [] };
+    const user: UserName = req.params.user;
+    let tDocEntries = [];
+    try {
+      tDocEntries = await getListOfUsersNotebooks(user);
+    } catch(err) {
+      switch(err.code) {
+      case 'ENOENT':
+        messages.error.push(`User '${user}' does not exist.<br/><tt>mkdir -p ~/math-tablet-usr/${user}</tt>`);
+        break;
+      default:
+        throw err;
+      }
+    }
+    res.render('user-home', { messages, user, tDocEntries });
+  } catch(err) {
+    console.error(err.message);
+    console.log(err.stack);
+    res.send(`Server crash: ${err.message}`);
+  }
 });
 
 router.get('/:user/:notebook', function(_req, res, _next) {
-  res.render('notebook', { credentials });
+  try {
+    // TODO: check that user exists.
+    // TODO: check that notebook exists?
+    res.render('notebook', { credentials });
+  } catch(err) {
+    console.error(err.message);
+    console.log(err.stack);
+    res.send(`Server crash: ${err.message}`);
+  }
 });
