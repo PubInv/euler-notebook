33438dd600f87c91f0784edb7ee179a9b0bdb23f
David Jeschke <david@xtramath.org>
2019-04-17 08:23:25 -0700

    Add AGPLv3 license to all source files

--- client/dom.ts
+++ client/dom.ts
@@ -1,9 +1,30 @@
+/*
+Math Tablet
+Copyright (C) 2019 Public Invention
+https://pubinv.github.io/PubInv/
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Types
 
 type CssSelector = string;
 type HtmlElementTag = string;
 export type HtmlElementClass = string;
 export type Html = string;
+
+// Exported Functions
 
 export function $<T extends HTMLElement>(selector: CssSelector): T {
   const $elt = document.querySelector(selector);
