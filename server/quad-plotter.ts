2a1f2b8eb424c17e94e22eb0c10ac7f1165f1828
Robert L. Read <read.robert@gmail.com>
2019-05-09 17:28:24 -0500

    adding classifier and draft quad-specific plotting

---
+++ server/quad-plotter.ts
@@ -0,0 +1,110 @@
+/*
+Math Tablet
+Copyright (C) 2019 Public Invention
+https://pubinv.github.io/PubInv/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Requirements
+
+import { StyleObject } from '../client/math-tablet-api';
+import { TDoc, TDocChange } from './tdoc';
+import { execute } from './wolframscript';
+
+// Exports
+
+export async function initialize(): Promise<void> {
+  TDoc.on('open', (tDoc: TDoc)=>{
+    tDoc.on('change', function(this: TDoc, change: TDocChange){ onChange(this, change); });
+    tDoc.on('close', function(this: TDoc){ onClose(this); });
+    onOpen(tDoc);
+  });
+}
+
+// Private Functions
+
+function onChange(tDoc: TDoc, change: TDocChange): void {
+  switch (change.type) {
+  case 'styleDeleted':
+    console.log(`QuadClassifier tDoc ${tDoc._path}/${change.type} change: `);
+    break;
+  case 'styleInserted':
+    console.log(`QuadClassifier tDoc ${tDoc._path}/${change.type} change: `);
+    quadClassifierRule(tDoc, change.style);
+    break;
+  case 'thoughtDeleted':
+    console.log(`QuadClassifier tDoc ${tDoc._path}/${change.type} change: `);
+    break;
+  case 'thoughtInserted':
+    console.log(`QuadClassifier tDoc ${tDoc._path}/${change.type} change: `);
+    break;
+  default:
+    console.log(`QuadClassifier tDoc unknown change: ${tDoc._path} ${(<any>change).type}`);
+    break;
+  }
+}
+
+function onClose(tDoc: TDoc): void {
+  console.log(`QuadClassifier tDoc close: ${tDoc._path}`);
+}
+
+function onOpen(tDoc: TDoc): void {
+  console.log(`QuadClassifier: tDoc open: ${tDoc._path}`);
+}
+
+async function plotQuadratic(expr : string,filename : string) : Promise<boolean> {
+  // Mathematica offers various ways to deal with this:
+  // https://reference.wolfram.com/language/tutorial/FindingTheStructureOfAPolynomial.html
+  // I believe this is a good invocation of an anonymous function
+  /*
+With[{v = Variables[#]},
+ Exponent[#, v[[1]]] == 2 && Length[v] == 1] &[x^2+x]
+With[{v = Variables[#1]},
+ Export[#2,Plot[#1,{v[[1]],0,6 Pi}]]]
+  */
+  const univariate_plot_script = `Export[#1,Plot[#2,`;
+  let result : string = await execute(quadratic_function_script+"&[" + expr + "]");
+  console.log("EXECUTE RESULTS",expr, result);
+  return result == "True";
+}
+
+export async function quadPlotterRule(tdoc: TDoc, style: StyleObject): Promise<StyleObject[]> {
+  if (style.type != 'CLASSIFICATION' || style.meaning != 'QUADRATIC') { return []; }
+  console.log("INSIDE QUAD PLOTTER :",style);
+  const targetPath = "./public/tmp";
+  const urlPath = "/tmp";
+  const fn = "quadplot" + "1" + ".gif";
+  const full_filename = targetPath + "/" + fn;
+
+  var createdPlotSuccessfully;
+  try {
+    createdPlotSuccessfully = await PlotQuadratic(style.data,full_filename);
+    console.log("QUAD CLASSIFER SAYS:",isPlottableQuadratic);
+  } catch (e) {
+    console.log("MATHEMATICA EVALUATION FAILED :",e);
+    return [];
+  }
+
+  var styles = [];
+  if (isPlottableQuadratic) {
+    var imageStyle =
+        tdoc.insertStyle(style,{ type: 'IMAGE',
+                                 data: urlPath+"/"+fn,
+                                 meaning: 'PLOT',
+                                 source: 'MATHEMATICA' })
+    styles.push(imageStyle);
+  }
+  return styles;
+}
