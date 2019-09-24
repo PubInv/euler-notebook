91d2e2919bd01096342856bb52688cae9073b0f5
Robert L. Read <read.robert@gmail.com>
2019-09-24 10:33:24 -0500

    fixing build bug (non-use)

--- client/cell-view.ts
+++ client/cell-view.ts
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Requirements
 
-import { assert, stackTrace } from './common.js';
+import { assert } from './common.js';
 import { escapeHtml, $new, Html } from './dom.js';
 import { getKatex } from './katex-types.js';
 import { NotebookView } from './notebook-view.js';
@@ -352,4 +352,3 @@ export class CellView {
   }
 
 }
-
